9406ae80a7ee74473ca7a0312039a2e201b75d7d
Ian <ian.davis@thomsonreuters.com>
2016-01-04 14:11:27 +0100

    Continue Re-arch with Jucator class for Player

---
+++ Ire/Jucator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ire
+{
+    class Jucator
+    {
+        GameCollection gc;
+        int Seed = -1;
+        int rating = -1;
+        string name = "unset";
+        string club = "unset";
+        string country = "unset";
+        float iMMS = -1;
+        float score =-1;
+        int[] position;
+        /*
+iMMS	float
+score	float
+position	intarray
+         */
+
+        public Jucator()
+        { }
+
+
+    }
+}
