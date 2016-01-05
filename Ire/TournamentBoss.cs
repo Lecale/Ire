440621719adb0c001c30ed50235f0953c6882b52
Ian <ian.davis@thomsonreuters.com>
2016-01-05 13:36:19 +0100

    Trudge

--- Ire/TournamentBoss.cs
+++ Ire/TournamentBoss.cs
@@ -15,6 +15,8 @@ namespace Ire
         string exeDirectory;
         string workDirectory;
         bool Macintosh = false;
+        List<Jucator> AllPlayers = new List<Jucator>();
+
 
         public TournamentBoss()
         {
@@ -34,6 +36,60 @@ namespace Ire
             using (ZipFile zip = ZipFile.Read(fi.FullName))
             {
                 zip.ExtractAll(workDirectory);
+            }
+        }
+
+        /*	
+         * Pin tName tRating tClub tCountry
+         * This method has a GIGO ideology
+		*/
+        public void ReadJucatori()
+        {
+           
+            string tLn = "";
+            string fin = workDirectory + "players.csv";
+            using (StreamReader reader = new StreamReader(fin))
+            {
+                for (int i = 0; i < 6; i++)
+                    tLn = reader.ReadLine(); //trip through headers
+
+                while ((tLn = reader.ReadLine()) != null)
+                {
+                    //tLn = reader.ReadLine();
+                    String[] split = tLn.Split(',');
+                    Console.WriteLine(split.Length);
+                    int rnd = split.Length - 5;
+                    Console.WriteLine("rnd" + rnd);
+
+                    try
+                    {
+                        int pine = int.Parse(split[0]);
+                        Console.WriteLine("pin" + pine);
+                        int rats = int.Parse(split[2]);
+                        Console.WriteLine("rat" + rats);
+                        bool[] bull = new bool[rnd];
+                        for (int i = 5; i < rnd + 5; i++)
+                        {
+                            if (split[i].Equals("") == false)
+                            {
+                                if ((split[i].Trim()).ToUpper().Equals("X"))
+                                    bull[i - 5] = false;
+                                else
+                                    bull[i - 5] = true;
+                            }
+                        }
+                        Jucator j = new Jucator(pine, split[1], rats, split[3], split[4], bull);
+                        if (AllPlayers.Contains(j) == false)
+                            AllPlayers.Add(j);
+                        else
+                            Console.WriteLine("Duplicate Entry detected");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Entry format error" + e.Message);
+                        Console.WriteLine(tLn);
+                    }
+                }
             }
         }
 #endregion
