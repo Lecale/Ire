176f3ba5ecf94d7a72d6962bdc902708a8478c48
Lecale <iandavis@Ians-MacBook-Pro.local>
2016-03-03 14:07:50 +0100

    Minor
    Add Person class as we continue to restructure this mess

---
+++ Ire/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ire
+{
+	public class Person
+	{
+		public string Name;
+		public string Clubtry;
+		public int Rating;
+		public int Seed;
+		public string Rank;
+
+		public Person (string _name, string _clubtry, string _rank, int _rating)
+		{
+			Name = _name;
+			Clubtry = _clubtry;
+			Rank = _rank;
+			Rating = _rating;
+		}
+
+		public Person (string _name, string _clubtry, string _rank, int _rating, int _seed)
+		{
+			Name = _name;
+			Clubtry = _clubtry;
+			Rank = _rank;
+			Rating = _rating;
+			Seed = _seed;
+		}
+
+		public void SetSeed(int s)
+		{
+			Seed = s;
+		}
+	}
+}
+
