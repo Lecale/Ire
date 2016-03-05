2f28c4f850f8f823a5c77e456a9cba89fe74338e
Lecale <iandavis@Ians-MacBook-Pro.local>
2016-03-05 20:57:30 +0100

    null
    n

---
+++ Ire/DrawMachine1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ire
+{
+	public class DrawMachine1 //Aka simple pairing
+	{
+		private List<Player> plys;
+		private int MaxHandi;
+		private int AdjHandi;
+		private bool HandiAboveBar;
+		private List<Pairing> Pairs;
+		private McLayer BigM;
+
+		public DrawMachine1 ( List<Player> ply, int _MaxHandi = 9, int _AdjHandi = 1, bool _HandiAboveBar = false)
+		{
+			plys = ply;
+			Pairs = new List<Pairing>();
+			MaxHandi = _MaxHandi;
+			AdjHandi = _AdjHandi;
+			HandiAboveBar = _HandiAboveBar;
+			plys.Sort (); //just in case
+			DRAW ();
+		}
+
+		//find top player
+		//take layer
+		//take player 'at random'
+		//if valid pair else retry
+		//   Next layer
+		//      Nothing? block and retry
+		public void DRAW()
+		{
+			
+		}
+
+	}
+}
+
+
