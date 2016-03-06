2e92bcd6f3d8f6d2ae03c28f1fef160b523d657b
Lecale <iandavis@Ians-MacBook-Pro.local>
2016-03-06 19:10:39 +0100

    minor
    things

--- Ire/DrawMachine1.cs
+++ Ire/DrawMachine1.cs
@@ -12,10 +12,12 @@ namespace Ire
 		private List<Pairing> Pairs;
 		private List<McLayer> BigM;
 		private List<Pairing> History = new List<Pairing>(); //previous rounds
+		private int[] LookUpTable;
 
 		public DrawMachine1 ( List<Player> ply, int _MaxHandi = 9, int _AdjHandi = 1, bool _HandiAboveBar = false)
 		{
 			plys = ply;
+			LookUpTable = new int[ply.Count];
 			Pairs = new List<Pairing>();
 			MaxHandi = _MaxHandi;
 			AdjHandi = _AdjHandi;
@@ -36,6 +38,8 @@ namespace Ire
 			//Shuffle
 			foreach (McLayer mcl in BigM)
 				mcl.Shuffle ();
+			for (int j = 0; j < LookUpTable.Length; j++)
+				LookUpTable [plys [j].Seed] = j; 
 			DRAW ();
 		}
 
@@ -49,12 +53,19 @@ namespace Ire
 		{
 			Player top = plys [0];
 			Pairing tmp;
-			List<Pairing> blocked = new List<Pairing> ();
+			Pairing holdLastPairing;
+			List<Pairing> Blocked = new List<Pairing> ();
 			bool found = false;
-			for (int i = 1; i < plys.Count - 1; i++) { //foreachplayer
+			for (int i = 1; i < plys.Count - 1; i++) { //foreachPlayer
+				found =false;
 				while (found == false) {
-					foreach (McLayer mcl in BigM) {
-					}
+					foreach (McLayer mcl in BigM) { //foreachLayer
+						for (int j = 0; j < mcl.Length; j++) {
+							tmp = new Pairing (plys[mcl.GetAt (j)], top);
+							if (History.Contains (tmp) == false && Blocked.Contains (tmp) == false)
+								found = true;
+						}
+					}//foreachLayer
 				}
 
 			}//end foreachplayer
