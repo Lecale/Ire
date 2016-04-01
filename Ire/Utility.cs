8cd155921fb8791bb441173099c00ef81c62d64c
Ian <ian.davis@thomsonreuters.com>
2016-04-01 14:19:48 +0200

    Utility - add results file filler

--- Ire/Utility.cs
+++ Ire/Utility.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Ire
 {
@@ -32,7 +34,48 @@ namespace Ire
 				d = d * -1;
 			return (int)(d * width + mid);
 		}
-			
+
+        public void EnterResults(string dir, int rnd)
+        {
+            List<string> allLines = new List<string>();
+            using (StreamReader sr = new StreamReader(dir + "Round" + rnd + "Results.txt"))
+            {
+                while (sr.EndOfStream == false)
+                    allLines.Add(sr.ReadLine());
+            }
+            using (StreamWriter sw = new StreamWriter(dir + "Round" + rnd + "Results.txt"))
+            {
+                int counter = 0;
+                foreach (string al in allLines)
+                {
+                    if (counter < 2)
+                        sw.WriteLine(al);
+                    else
+                    {
+                        if (r.NextDouble() < 0.45)
+                        {
+                            sw.WriteLine(al.Replace("?:?", "1:0"));
+                        }
+                        else
+                        {
+                            if (r.NextDouble() < 0.9)
+                            {
+                                sw.WriteLine(al.Replace("?:?", "0:1"));
+                            }
+                            else 
+                            {
+                                if (r.NextDouble() < 0.98)
+                                    sw.WriteLine(al.Replace("?:?", "0.5:0.5"));
+                                else
+                                    sw.WriteLine(al.Replace("?:?", "0:0")); 
+                            }
+                        } 
+                    }
+                        counter++;
+                }
+            }
+
+        }
 	}
 }
 
