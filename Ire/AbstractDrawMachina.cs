018a9a6b7a9cb1b5cb9b7d53009dd9278ad863bf
Lecale <iandavis@Ians-MacBook-Pro.local>
2016-04-12 12:22:11 +0200

    factory pattern
    very small step in creating this

---
+++ Ire/AbstractDrawMachina.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ire
+{
+	public abstract class AbstractDrawMachina
+	{
+		public AbstractDrawMachina(){
+		}
+
+		/*
+		 public AbstractDrawMachina (List<Player> ply, List<Pairing> _History, 
+			int _MaxHandi = 9, int _AdjHandi = 1, bool _HandiAboveBar = false)
+		{
+		}*/
+	}
+
+	public static class DrawFactory
+	{
+		/// <summary>
+		/// Decides which class to instantiate.
+		/// </summary>
+		public static AbstractDrawMachina Get(string abstraction, List<Player> ply, List<Pairing> _History, 
+			int _MaxHandi = 9, int _AdjHandi = 1, bool _HandiAboveBar = false)
+		{
+			switch (abstraction)
+			{
+			case "Simple":
+				return new DrawMachine1(ply,_History,_MaxHandi,_AdjHandi,_HandiAboveBar); 
+			case "Split":
+				return null;
+			case "Fold":
+			default:
+				return null;
+			}
+		}
+	}
+}
+
