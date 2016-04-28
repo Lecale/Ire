58586dd15f85f2306e96b2587e48cdabca00c075
Ian <ian.davis@thomsonreuters.com>
2016-04-28 17:30:25 +0200

    Tidy Program class

--- Ire/Program.cs
+++ Ire/Program.cs
@@ -12,13 +12,19 @@ namespace Ire
         {
 
             TournamentBoss tb = new TournamentBoss(true);
+            Console.WriteLine("Do you need to download the EGF Rating List? (yes/no)");
+            string s = Console.ReadLine();
+            if (s.ToUpper().StartsWith("Y"))
+            {
+                tb.DownloadEGFMasterZip();
+            }
 			tb.GenerateTemplateInputFile ();
 			tb.GeneratePlayers (); // Cut to become auto
 			tb.ReadPlayers();
 			tb.SortField(true);
 			tb.ShowField ();
-			Console.WriteLine ("Ready to read in Settings File? (yes/no)");
-			string s = Console.ReadLine();//wait	
+			Console.WriteLine ("Press any key to read in the Settings File");
+			s = Console.ReadLine();//wait	
 			tb.ReadSettings ();
 			tb.previewTopBar (true);
 			tb.previewFloor (true);
@@ -27,7 +33,6 @@ namespace Ire
             for (int i = 1; i < rounds+1; i++)
             {
                 tb.MakeDraw(i);
-         //       tb.GenerateResultsForRound(i);
                 tb.ReadResults(i);
           //      tb.ShowField("Round" + i + "AfterReadResults"); //just a debug method
                 tb.ProcessResults(i);
@@ -39,22 +44,28 @@ namespace Ire
             }
             tb.GenerateEGFExport();
             tb.ConvertStandingsToHTML(rounds);
+
+            Console.WriteLine("The tournament has ended.");
+            Console.WriteLine("We hope you enjoyed using Ire to make the pairings.");
+            Console.WriteLine(RandomAdvice());
+        }
+
+        static string RandomAdvice()
+        {
+            Random d = new Random();
+            List<string> FortuneCookie = new List<string>();
+            FortuneCookie.Add("Go home and rest.");
+            FortuneCookie.Add("Go out and party.");
+            FortuneCookie.Add("Go forth and conquer.");
+            FortuneCookie.Add("Now it is time to eat.");
+            FortuneCookie.Add("Consider the Lily.");
+            FortuneCookie.Add("Have you ever thought about changing your life?");
+            FortuneCookie.Add("The way back to Earth has been wiped from my memory.");
+            FortuneCookie.Add("Leave no prisoners behind.");
+            FortuneCookie.Add("Tomorrow is a new day.");
+            FortuneCookie.Add("Dispatch war rocket Ajax to collect his body.");
             
-			/*
-			i.DownloadMasterZipEGF ();
-            i.GenerateTemplate();
-			Console.ReadLine ();
-            Turn t = new Turn(i._Round);
-            Console.WriteLine("importing players 1...");
-            List<Player> pImport = i.ReadPlayers();
-            foreach (Player pp in pImport)
-                Console.WriteLine(pp.ToString());
-            Console.WriteLine("importing players 2...");
-            t.AddPlayersFromImport(pImport);
-			t.DebugPlayers ();
-            t.SetBar();
-            t.MakePairing();
-*/
+            return "";
         }
     }
 }
